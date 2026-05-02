e7d0a36370c94c40f1be65e738700d65cd94f680
seyid12 <seyid12@users.noreply.github.com>
2026-05-02 19:16:15 +0300

    feat: support FIREBASE_CREDENTIALS_JSON env variable for Render.com deployment

--- ReelEnglish_backend/internal/config/firebase.go
+++ ReelEnglish_backend/internal/config/firebase.go
@@ -3,6 +3,7 @@ package config
 import (
 	"context"
 	"log"
+	"os"
 
 	firebase "firebase.google.com/go/v4"
 	"google.golang.org/api/option"
@@ -11,13 +12,25 @@ import (
 // FirebaseApp başlatılmış Firebase uygulamasını tutar
 var FirebaseApp *firebase.App
 
-// InitFirebase Firebase Admin SDK'yı başlatır
+// InitFirebase Firebase Admin SDK'yı başlatır.
+// Render.com gibi ortamlarda FIREBASE_CREDENTIALS_JSON ortam değişkeninden okur.
+// Lokal geliştirmede serviceAccountKey.json dosyasını kullanır.
 func InitFirebase() error {
-	// Arka plan context'i
 	ctx := context.Background()
 
-	// serviceAccountKey.json dosyasını kullanarak kimlik doğrulaması yap
-	opt := option.WithCredentialsFile("serviceAccountKey.json")
+	var opt option.ClientOption
+
+	// 1. Önce ortam değişkenini kontrol et (Render.com için)
+	credJSON := os.Getenv("FIREBASE_CREDENTIALS_JSON")
+	if credJSON != "" {
+		log.Println("Firebase: FIREBASE_CREDENTIALS_JSON ortam değişkeninden kimlik doğrulanıyor...")
+		opt = option.WithCredentialsJSON([]byte(credJSON))
+	} else {
+		// 2. Ortam değişkeni yoksa lokal dosyayı kullan (geliştirme için)
+		log.Println("Firebase: serviceAccountKey.json dosyasından kimlik doğrulanıyor...")
+		opt = option.WithCredentialsFile("serviceAccountKey.json")
+	}
+
 	app, err := firebase.NewApp(ctx, nil, opt)
 	if err != nil {
 		return err
